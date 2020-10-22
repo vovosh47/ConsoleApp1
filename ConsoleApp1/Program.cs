31e7a2423aaf7d477a69570138f6d5fac977a7fa
Argon <70173258+ArGoNArGoN@users.noreply.github.com>
2020-10-22 04:03:01 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("dawdawdaw");
             Console.WriteLine("dawdawdaw");
+
+            Console.WriteLine("dawdaw");
         }
     }
 }
